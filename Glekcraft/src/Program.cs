9435f0ac67fe2654a9c63d793b1e83a103732d8d
G'lek Tarssza <glek@glektarssza.com>
2022-12-14 11:00:11 -0700

    Adjusted program entry point.

--- Glekcraft/src/Program.cs
+++ Glekcraft/src/Program.cs
@@ -1,2 +1,13 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Glekcraft;
+
+/// <summary>
+/// The class containing the program entry point.
+/// </summary>
+public sealed class Program {
+    /// <summary>
+    /// The program entry point.
+    /// </summary>
+    public static void Main() {
+        Console.WriteLine("Hello, world!");
+    }
+}
