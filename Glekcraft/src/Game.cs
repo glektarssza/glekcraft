97a5e1467911af3fdcae9a3761ebe252770e36f7
G'lek Tarssza <glek@glektarssza.com>
2022-12-19 13:37:12 -0700

    Doing some refactoring.

---
+++ Glekcraft/src/Game.cs
@@ -0,0 +1,147 @@
+namespace Glekcraft;
+
+using Silk.NET.Input;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+
+/// <summary>
+/// The main game class.
+/// </summary>
+public sealed class Game : IDisposable {
+    #region Public Properties
+
+    /// <summary>
+    /// The main input context.
+    /// </summary>
+    public IInputContext? Input {
+        get;
+        private set;
+    }
+
+    /// <summary>
+    /// The OpenGL context.
+    /// </summary>
+    public GL? GL {
+        get;
+        private set;
+    }
+
+    /// <summary>
+    /// The main game window.
+    /// </summary>
+    public IWindow? Window {
+        get;
+        private set;
+    }
+
+    /// <summary>
+    /// Whether this instance has been disposed.
+    /// </summary>
+    public bool IsDisposed {
+        get;
+        private set;
+    }
+
+    #endregion
+
+    #region Constructors/Finalizer
+
+    /// <summary>
+    /// Create a new instance.
+    /// </summary>
+    public Game() {
+        //-- Does nothing
+    }
+
+    /// <summary>
+    /// The finalizer.
+    /// </summary>
+    ~Game() =>
+        Dispose(false);
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Initialize this instance.
+    /// </summary>
+    public void Initialize() {
+        var windowOptions = WindowOptions.Default;
+        windowOptions.Size = new(640, 480);
+        windowOptions.Title = "Glekcraft";
+        windowOptions.WindowClass = "Glekcraft";
+        windowOptions.WindowBorder = WindowBorder.Resizable;
+        windowOptions.WindowState = WindowState.Normal;
+        windowOptions.IsVisible = false;
+        windowOptions.FramesPerSecond = 60;
+        windowOptions.UpdatesPerSecond = 60;
+        windowOptions.ShouldSwapAutomatically = false;
+        windowOptions.Samples = 1;
+        Window = Silk.NET.Windowing.Window.Create(windowOptions);
+        Window.Load += () => {
+            Window.Center();
+            Window.IsVisible = true;
+            Input = Window.CreateInput();
+            GL = GL.GetApi(Window);
+        };
+        Window.Update += (deltaTime) => {
+            for (var i = 0; i < (Input?.Keyboards.Count ?? 0); i++) {
+                var keyboard = Input?.Keyboards[i];
+                if (keyboard?.IsKeyPressed(Key.Escape) ?? false) {
+                    Window.Close();
+                }
+            }
+        };
+        Window.Render += (deltaTime) => {
+            GL?.Viewport(0, 0, (uint)Window.Size.X, (uint)Window.Size.Y);
+            GL?.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            // TODO
+            Window.SwapBuffers();
+        };
+        Window.Closing += () => {
+            Input?.Dispose();
+            Input = null;
+            GL?.Dispose();
+            GL = null;
+        };
+    }
+
+    /// <summary>
+    /// Run the game.
+    /// </summary>
+    public void Run() =>
+        Window?.Run();
+
+    /// <summary>
+    /// Dispose this instance.
+    /// </summary>
+    public void Dispose() {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Dispose this instance.
+    /// </summary>
+    /// <param name="managed">
+    /// Whether this method is being called from managed code or from unmanaged
+    /// code (e.g. the garbage collector).
+    /// </param>
+    private void Dispose(bool managed) {
+        if (IsDisposed) {
+            return;
+        }
+        if (managed) {
+            Window?.Dispose();
+            Window = null;
+        }
+        IsDisposed = true;
+    }
+
+    #endregion
+}
--- Glekcraft/src/Program.cs
+++ Glekcraft/src/Program.cs
@@ -14,45 +14,9 @@ public sealed class Program {
     /// The program entry point.
     /// </summary>
     public static void Main() {
-        var windowOptions = WindowOptions.Default;
-        windowOptions.Size = new(640, 480);
-        windowOptions.Title = "Glekcraft";
-        windowOptions.WindowClass = "Glekcraft";
-        windowOptions.WindowBorder = WindowBorder.Resizable;
-        windowOptions.WindowState = WindowState.Normal;
-        windowOptions.IsVisible = false;
-        windowOptions.FramesPerSecond = 60;
-        windowOptions.UpdatesPerSecond = 60;
-        windowOptions.ShouldSwapAutomatically = false;
-        windowOptions.Samples = 1;
-        var window = Window.Create(windowOptions);
-        IInputContext? input = null;
-        GL? gl = null;
-        window.Load += () => {
-            window.Center();
-            window.IsVisible = true;
-            input = window.CreateInput();
-            gl = GL.GetApi(window);
-            gl.ClearColor(Color.CornflowerBlue);
-        };
-        window.Update += (deltaTime) => {
-            for (var i = 0; i < (input?.Keyboards.Count ?? 0); i++) {
-                var keyboard = input?.Keyboards[i];
-                if (keyboard?.IsKeyPressed(Key.Escape) ?? false) {
-                    window.Close();
-                }
-            }
-        };
-        window.Render += (deltaTime) => {
-            gl?.Viewport(0, 0, (uint)window.Size.X, (uint)window.Size.Y);
-            gl?.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            // TODO
-            window.SwapBuffers();
-        };
-        window.Closing += () => {
-            gl?.Dispose();
-            gl = null;
-        };
-        window.Run();
+        using var game = new Game();
+        game.Initialize();
+        game.Run();
+        game.Dispose();
     }
 }
