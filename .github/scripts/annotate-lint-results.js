cbb113fe9e29709b18a1bd5e55cea3b78a6d6fa7
G'lek Tarssza <glek@glektarssza.com>
2023-12-18 14:25:41 -0700

    Need to provide end line.

--- .github/scripts/annotate-lint-results.js
+++ .github/scripts/annotate-lint-results.js
@@ -24,6 +24,7 @@ module.exports = async ({ github, context, glob, result }) => {
             }
             return {
                 start_line: change.LineNumber,
+                end_line: change.LineNumber,
                 start_column: change.CharNumber,
                 path: file.FilePath,
                 annotation_level,
