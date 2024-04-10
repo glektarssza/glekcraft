7976fb29fef28517cbea9b897a4d181da4284ff6
G'lek Tarssza <glek@glektarssza.com>
2024-04-10 13:30:44 -0600

    Setup basic project structure.

---
+++ Glekcraft/src/Program.cs
@@ -0,0 +1,11 @@
+namespace Glekcraft;
+
+/// <summary>
+/// The class containing the program entry point.
+/// </summary>
+public static class Program {
+    public static int Main() {
+        Console.WriteLine("Hello world!");
+        return 0;
+    }
+}
