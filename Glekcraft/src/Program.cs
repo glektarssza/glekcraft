8811df7601ac0e7937ac072345d6f1dcc279c7a7
G'lek Tarssza <glek@glektarssza.com>
2024-04-23 12:26:08 -0600

    Drawing triangle.

--- Glekcraft/src/Program.cs
+++ Glekcraft/src/Program.cs
@@ -1,6 +1,7 @@
 namespace Glekcraft;
 
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using Silk.NET.Input;
 using Silk.NET.OpenGL;
@@ -10,6 +11,44 @@ using Silk.NET.Windowing;
 /// The class containing the program entry point.
 /// </summary>
 public static class Program {
+    /// <summary>
+    /// The vertex array object.
+    /// </summary>
+    public static uint VAO {
+        get;
+        private set;
+    }
+
+    public static uint VertexBuffer {
+        get;
+        private set;
+    }
+
+    public static uint ColorBuffer {
+        get;
+        private set;
+    }
+
+    public static uint IndexBuffer {
+        get;
+        private set;
+    }
+
+    public static uint VertexShader {
+        get;
+        private set;
+    }
+
+    public static uint FragmentShader {
+        get;
+        private set;
+    }
+
+    public static uint ShaderProgram {
+        get;
+        private set;
+    }
+
     /// <summary>
     /// The main game window.
     /// </summary>
@@ -92,6 +131,75 @@ public static class Program {
         MainWindow.Size = new(1280, 720);
         MainWindow.Center();
         MainWindow.IsVisible = true;
+
+        MainWindow.MakeCurrent();
+        VAO = GraphicsContext.GenVertexArray();
+        GraphicsContext.BindVertexArray(VAO);
+        VertexBuffer = GraphicsContext.GenBuffer();
+        GraphicsContext.BindBuffer(BufferTargetARB.ArrayBuffer, VertexBuffer);
+        GraphicsContext.BufferData(BufferTargetARB.ArrayBuffer, [
+            -0.5f, -0.5f, 0f,
+            0.5f, -0.5f, 0f,
+            0f, 0.5f, 0f
+        ], BufferUsageARB.StaticDraw);
+
+        ColorBuffer = GraphicsContext.GenBuffer();
+        GraphicsContext.BindBuffer(BufferTargetARB.ArrayBuffer, ColorBuffer);
+        GraphicsContext.BufferData<byte>(BufferTargetARB.ArrayBuffer, [
+            255, 0, 0,
+            0, 255, 0,
+            0, 0, 255
+        ], BufferUsageARB.StaticDraw);
+
+        IndexBuffer = GraphicsContext.GenBuffer();
+        GraphicsContext.BindBuffer(BufferTargetARB.ElementArrayBuffer, IndexBuffer);
+        GraphicsContext.BufferData<ushort>(BufferTargetARB.ElementArrayBuffer, [
+            0, 1, 2
+        ], BufferUsageARB.StaticDraw);
+
+        VertexShader = GraphicsContext.CreateShader(ShaderType.VertexShader);
+        GraphicsContext.ShaderSource(VertexShader, @"
+#version 410
+
+layout(location = 0) in vec3 vs_vertex;
+layout(location = 1) in vec3 vs_color;
+
+layout(location = 0) out vec3 fs_color;
+
+void main() {
+    fs_color = vs_color;
+    gl_Position = vec4(vs_vertex, 1.0);
+}
+");
+        GraphicsContext.CompileShader(VertexShader);
+
+        Console.Out.WriteLine("=== Vertex Shader Info Log ===");
+        Console.Out.WriteLine(GraphicsContext.GetShaderInfoLog(VertexShader));
+
+        FragmentShader = GraphicsContext.CreateShader(ShaderType.FragmentShader);
+        GraphicsContext.ShaderSource(FragmentShader, @"
+#version 410
+
+layout(location = 0) in vec3 fs_color;
+
+out vec4 fragColor;
+
+void main() {
+    fragColor = vec4(fs_color, 1.0);
+}
+");
+        GraphicsContext.CompileShader(FragmentShader);
+
+        Console.Out.WriteLine("=== Fragment Shader Info Log ===");
+        Console.Out.WriteLine(GraphicsContext.GetShaderInfoLog(FragmentShader));
+
+        ShaderProgram = GraphicsContext.CreateProgram();
+        GraphicsContext.AttachShader(ShaderProgram, VertexShader);
+        GraphicsContext.AttachShader(ShaderProgram, FragmentShader);
+        GraphicsContext.LinkProgram(ShaderProgram);
+
+        Console.Out.WriteLine("=== Shader Program Info Log ===");
+        Console.Out.WriteLine(GraphicsContext.GetProgramInfoLog(ShaderProgram));
     }
 
     public static void OnWindowUpdate(double delta) {
@@ -112,8 +220,27 @@ public static class Program {
             throw new InvalidOperationException();
         }
         MainWindow.MakeCurrent();
+        _ = GraphicsContext.GetError();
         GraphicsContext.Viewport(MainWindow.FramebufferSize);
         GraphicsContext.ClearColor(Color.CornflowerBlue);
         GraphicsContext.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+        GraphicsContext.UseProgram(ShaderProgram);
+        GraphicsContext.BindVertexArray(VAO);
+        GraphicsContext.EnableVertexAttribArray(0);
+        GraphicsContext.EnableVertexAttribArray(1);
+        GraphicsContext.BindBuffer(BufferTargetARB.ArrayBuffer, VertexBuffer);
+        unsafe {
+            GraphicsContext.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, null);
+        }
+        GraphicsContext.BindBuffer(BufferTargetARB.ArrayBuffer, ColorBuffer);
+        unsafe {
+            GraphicsContext.VertexAttribPointer(1, 3, VertexAttribPointerType.UnsignedByte, true, 0, null);
+        }
+        GraphicsContext.BindBuffer(BufferTargetARB.ElementArrayBuffer, IndexBuffer);
+        unsafe {
+            GraphicsContext.DrawElements(PrimitiveType.Triangles, 3, DrawElementsType.UnsignedShort, null);
+        }
+        GraphicsContext.DisableVertexAttribArray(1);
+        GraphicsContext.DisableVertexAttribArray(0);
     }
 }
