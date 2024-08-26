4ae30516aec8b121a145eb6f9c8e5d1300cf2a7b
G'lek Tarssza <glek@glektarssza.com>
2024-08-26 12:49:55 -0600

    Added a base project.

---
+++ Glekcraft/src/Program.cs
@@ -0,0 +1,17 @@
+namespace Glekcraft;
+
+/// <summary>
+/// The class containing the program entry point.
+/// </summary>
+public static class Program {
+    /// <summary>
+    /// The program entry point.
+    /// </summary>
+    /// <returns>
+    /// A status code indicating whether the program completed successfully.
+    /// </returns>
+    public static int Main() {
+        Console.WriteLine("Hello, world!");
+        return 0;
+    }
+}
