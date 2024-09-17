b4737935790212a7f80d6aa643cf06a3ba53fb42
G'lek Tarssza <glek@glektarssza.com>
2024-09-17 08:33:32 -0600

    Doing some cleanup.

--- Glekcraft/src/Program.cs
+++ Glekcraft/src/Program.cs
@@ -1,7 +1,5 @@
 namespace Glekcraft;
 
-using Glekcraft.Graphics.GLFW;
-
 /// <summary>
 /// The class containing the program entry point.
 /// </summary>
@@ -13,15 +11,7 @@ public static class Program {
     /// A status code indicating whether the program completed successfully.
     /// </returns>
     public static int Main() {
-        using var glfw = LibGLFW.Initialize();
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Using GLFW v{0}", glfw.NativeVersionString);
-        var monitors = glfw.Monitors;
-        Console.WriteLine("Found {0} monitors", monitors.Length);
-        Console.WriteLine("=== Monitors ===");
-        foreach (var monitor in monitors) {
-            Console.WriteLine("{0} {1}", monitor.IsPrimary ? "*" : " ", monitor.Name ?? "Unknown");
-        }
         return (int)ExitCode.Success;
     }
 }
