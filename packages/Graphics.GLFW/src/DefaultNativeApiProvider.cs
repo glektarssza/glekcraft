0cde2d47876681be79dc0f79e15e51868480bebd
G'lek Tarssza <glek@glektarssza.com>
2024-09-26 12:10:57 -0600

    Added monitor native APIs.

--- packages/Graphics.GLFW/src/DefaultNativeApiProvider.cs
+++ packages/Graphics.GLFW/src/DefaultNativeApiProvider.cs
@@ -52,6 +52,55 @@ internal partial class DefaultNativeApiProvider : INativeApiProvider {
 
     #endregion
 
+    #region External APIs - Monitors
+
+    [LibraryImport(LibraryName)]
+    private static partial IntPtr glfwGetMonitors(ref int count);
+
+    [LibraryImport(LibraryName)]
+    private static partial IntPtr glfwGetPrimaryMonitor();
+
+    [LibraryImport(LibraryName)]
+    private static partial void glfwGetMonitorPos(IntPtr monitor, ref int xPos, ref int yPos);
+
+    [LibraryImport(LibraryName)]
+    private static partial void glfwGetMonitorWorkarea(IntPtr monitor, ref int xPos, ref int yPos, ref int width, ref int height);
+
+    [LibraryImport(LibraryName)]
+    private static partial void glfwGetMonitorPhysicalSize(IntPtr monitor, ref int widthMM, ref int heightMM);
+
+    [LibraryImport(LibraryName)]
+    private static partial void glfwGetMonitorContentScale(IntPtr monitor, ref float xScale, ref float yScale);
+
+    [LibraryImport(LibraryName)]
+    private static partial IntPtr glfwGetMonitorName(IntPtr monitor);
+
+    [LibraryImport(LibraryName)]
+    private static partial void glfwSetMonitorUserPointer(IntPtr monitor, IntPtr pointer);
+
+    [LibraryImport(LibraryName)]
+    private static partial IntPtr glfwGetMonitorUserPointer(IntPtr monitor);
+
+    [LibraryImport(LibraryName)]
+    private static partial INativeApiProvider.MonitorCallback? glfwSetMonitorCallback(INativeApiProvider.MonitorCallback? callback);
+
+    [LibraryImport(LibraryName)]
+    private static partial IntPtr glfwGetVideoModes(IntPtr monitor, ref int count);
+
+    [LibraryImport(LibraryName)]
+    private static partial IntPtr glfwGetVideoMode(IntPtr monitor);
+
+    [LibraryImport(LibraryName)]
+    private static partial void glfwSetGamma(IntPtr monitor, float gamma);
+
+    [LibraryImport(LibraryName)]
+    private static partial IntPtr glfwGetGammaRamp(IntPtr monitor);
+
+    [LibraryImport(LibraryName)]
+    private static partial void glfwSetGammaRamp(IntPtr monitor, IntPtr ramp);
+
+    #endregion
+
     #region Public Methods - Core
 
     /// <inheritdoc />
@@ -95,6 +144,169 @@ internal partial class DefaultNativeApiProvider : INativeApiProvider {
 
     /// <inheritdoc />
     public bool IsPlatformSupported(Platform platform) => glfwPlatformSupported(platform);
+
+    #endregion
+
+    #region Public Methods - Monitors
+
+    /// <inheritdoc />
+    public IntPtr[] GetMonitors() {
+        var count = 0;
+        var monitorsPtr = glfwGetMonitors(ref count);
+        if (monitorsPtr == IntPtr.Zero) {
+            return [];
+        }
+        var monitors = new IntPtr[count];
+        for (var i = 0; i < count; i++) {
+            monitors[i] = Marshal.ReadIntPtr(monitorsPtr, i * IntPtr.Size);
+        }
+        return monitors;
+    }
+
+    /// <inheritdoc />
+    public IntPtr GetPrimaryMonitor() => glfwGetPrimaryMonitor();
+
+    /// <inheritdoc />
+    public (int xPos, int yPos)? GetMonitorPosition(IntPtr monitor) {
+        if (monitor == IntPtr.Zero) {
+            return null;
+        }
+        int xPos = -1, yPos = -1;
+        glfwGetMonitorPos(monitor, ref xPos, ref yPos);
+        return xPos == -1 || yPos == -1 ? null : (xPos, yPos);
+    }
+
+    /// <inheritdoc />
+    public (int xPos, int yPos, int width, int height)? GetMonitorWorkArea(IntPtr monitor) {
+        if (monitor == IntPtr.Zero) {
+            return null;
+        }
+        int xPos = -1, yPos = -1, width = -1, height = -1;
+        glfwGetMonitorWorkarea(monitor, ref xPos, ref yPos, ref width, ref height);
+        return xPos == -1 || yPos == -1 || width == -1 || height == -1 ? null : (xPos, yPos, width, height);
+    }
+
+    /// <inheritdoc />
+    public (int widthMM, int heightMM)? GetMonitorPhysicalSize(IntPtr monitor) {
+        if (monitor == IntPtr.Zero) {
+            return null;
+        }
+        int widthMM = -1, heightMM = -1;
+        glfwGetMonitorPhysicalSize(monitor, ref widthMM, ref heightMM);
+        return widthMM == -1 || heightMM == -1 ? null : (widthMM, heightMM);
+    }
+
+    /// <inheritdoc />
+    public (float xScale, float yScale)? GetMonitorContentScale(IntPtr monitor) {
+        if (monitor == IntPtr.Zero) {
+            return null;
+        }
+        float xScale = -1, yScale = -1;
+        glfwGetMonitorContentScale(monitor, ref xScale, ref yScale);
+        return xScale == -1 || yScale == -1 ? null : (xScale, yScale);
+    }
+
+    /// <inheritdoc />
+    public string? GetMonitorName(IntPtr monitor) {
+        if (monitor == IntPtr.Zero) {
+            return null;
+        }
+        return Marshal.PtrToStringUTF8(glfwGetMonitorName(monitor));
+    }
+
+    /// <inheritdoc />
+    public void SetMonitorUserPointer(IntPtr monitor, IntPtr pointer) {
+        if (monitor == IntPtr.Zero) {
+            return;
+        }
+        glfwSetMonitorUserPointer(monitor, pointer);
+    }
+
+    /// <inheritdoc />
+    public IntPtr GetMonitorUserPointer(IntPtr monitor) {
+        if (monitor == IntPtr.Zero) {
+            return IntPtr.Zero;
+        }
+        return glfwGetMonitorUserPointer(monitor);
+    }
+
+    /// <inheritdoc />
+    public INativeApiProvider.MonitorCallback? SetMonitorCallback(INativeApiProvider.MonitorCallback? callback) =>
+        glfwSetMonitorCallback(callback);
+
+    /// <inheritdoc />
+    public VideoMode[] GetVideoModes(IntPtr monitor) {
+        if (monitor == IntPtr.Zero) {
+            return [];
+        }
+        var count = 0;
+        var videoModesPtr = glfwGetVideoModes(monitor, ref count);
+        if (videoModesPtr == IntPtr.Zero) {
+            return [];
+        }
+        var videoModes = new VideoMode[count];
+        for (var i = 0; i < count; i++) {
+            videoModes[i] = Marshal.PtrToStructure<VideoMode>(Marshal.ReadIntPtr(videoModesPtr, i * IntPtr.Size));
+        }
+        return videoModes;
+    }
+
+    /// <inheritdoc />
+    public VideoMode? GetVideoMode(IntPtr monitor) {
+        if (monitor == IntPtr.Zero) {
+            return null;
+        }
+        return Marshal.PtrToStructure<VideoMode>(glfwGetVideoMode(monitor));
+    }
+
+    /// <inheritdoc />
+    public void SetGamma(IntPtr monitor, float gamma) {
+        if (monitor == IntPtr.Zero) {
+            return;
+        }
+        glfwSetGamma(monitor, gamma);
+    }
+
+    /// <inheritdoc />
+    public GammaRamp? GetGammaRamp(IntPtr monitor) {
+        if (monitor == IntPtr.Zero) {
+            return null;
+        }
+        return Marshal.PtrToStructure<GammaRamp>(glfwGetGammaRamp(monitor));
+    }
+
+    /// <inheritdoc />
+    public void SetGammaRamp(IntPtr monitor, GammaRamp ramp) {
+        if (monitor == IntPtr.Zero) {
+            return;
+        }
+        var tempRedArray = new short[ramp.Size];
+        Buffer.BlockCopy(ramp.Red, 0, tempRedArray, 0, (int)ramp.Size);
+        var tempGreenArray = new short[ramp.Size];
+        Buffer.BlockCopy(ramp.Green, 0, tempGreenArray, 0, (int)ramp.Size);
+        var tempBlueArray = new short[ramp.Size];
+        Buffer.BlockCopy(ramp.Blue, 0, tempBlueArray, 0, (int)ramp.Size);
+
+        var redPtr = Marshal.AllocHGlobal(Marshal.SizeOf<ushort>() * (int)ramp.Size);
+        var greenPtr = Marshal.AllocHGlobal(Marshal.SizeOf<ushort>() * (int)ramp.Size);
+        var bluePtr = Marshal.AllocHGlobal(Marshal.SizeOf<ushort>() * (int)ramp.Size);
+        var rampPtr = Marshal.AllocHGlobal(Marshal.SizeOf<GammaRamp>());
+
+        Marshal.Copy(tempRedArray, 0, redPtr, (int)ramp.Size);
+        Marshal.Copy(tempGreenArray, 0, greenPtr, (int)ramp.Size);
+        Marshal.Copy(tempBlueArray, 0, bluePtr, (int)ramp.Size);
+        Marshal.WriteIntPtr(rampPtr, IntPtr.Size * 0, redPtr);
+        Marshal.WriteIntPtr(rampPtr, IntPtr.Size * 1, greenPtr);
+        Marshal.WriteIntPtr(rampPtr, IntPtr.Size * 2, bluePtr);
+        Marshal.WriteInt64(rampPtr, IntPtr.Size * 3, ramp.Size);
+
+        glfwSetGammaRamp(monitor, rampPtr);
+
+        Marshal.FreeHGlobal(redPtr);
+        Marshal.FreeHGlobal(greenPtr);
+        Marshal.FreeHGlobal(bluePtr);
+        Marshal.FreeHGlobal(rampPtr);
+    }
 
     #endregion
 }
--- packages/Graphics.GLFW/src/INativeApiProvider.cs
+++ packages/Graphics.GLFW/src/INativeApiProvider.cs
@@ -18,6 +18,18 @@ public interface INativeApiProvider {
     /// </param>
     public delegate void ErrorCallback(ErrorCode code, string? description);
 
+    /// <summary>
+    /// A callback that is invoked when a monitor is connected or disconnected
+    /// from the system.
+    /// </summary>
+    /// <param name="monitor">
+    /// The handle to the monitor which was connected or disconnected.
+    /// </param>
+    /// <param name="event">
+    /// The event which occurred.
+    /// </param>
+    public delegate void MonitorCallback(IntPtr monitor, MonitorEvent @event);
+
     #endregion
 
     #region Public Methods - Core
@@ -117,6 +129,172 @@ public interface INativeApiProvider {
     /// <c>false</c> otherwise.
     /// </returns>
     public bool IsPlatformSupported(Platform platform);
+
+    #endregion
+
+    #region Public Methods - Monitors
+
+    /// <summary>
+    /// Get a list of monitor handles connected to the system.
+    /// </summary>
+    /// <returns>
+    /// A list of monitor handles connected to the system.
+    /// </returns>
+    public IntPtr[] GetMonitors();
+
+    /// <summary>
+    /// Get the handle to the primary monitor.
+    /// </summary>
+    /// <returns>
+    /// The handle to the primary monitor.
+    /// </returns>
+    public IntPtr GetPrimaryMonitor();
+
+    /// <summary>
+    /// Get the position of a monitor in the virtual desktop coordinate system.
+    /// </summary>
+    /// <param name="monitor">
+    /// The handle to the monitor to get the position of.
+    /// </param>
+    /// <returns>
+    /// The position of the monitor in the virtual desktop coordinate system.
+    /// </returns>
+    public (int xPos, int yPos)? GetMonitorPosition(IntPtr monitor);
+
+    /// <summary>
+    /// Get the work area of a monitor in the virtual desktop coordinate system.
+    /// </summary>
+    /// <param name="monitor">
+    /// The handle to the monitor to get the work area of.
+    /// </param>
+    /// <returns>
+    /// The work area of the monitor in the virtual desktop coordinate system.
+    /// </returns>
+    public (int xPos, int yPos, int width, int height)? GetMonitorWorkArea(IntPtr monitor);
+
+    /// <summary>
+    /// Get the physical size of a monitor, in millimeters.
+    /// </summary>
+    /// <param name="monitor">
+    /// The handle to the monitor to get the physical size of.
+    /// </param>
+    /// <returns>
+    /// The physical size of the monitor, in millimeters.
+    /// </returns>
+    public (int widthMM, int heightMM)? GetMonitorPhysicalSize(IntPtr monitor);
+
+    /// <summary>
+    /// Get the content scale of a monitor.
+    /// </summary>
+    /// <param name="monitor">
+    /// The handle to the monitor to get the content scale of.
+    /// </param>
+    /// <returns>
+    /// The content scale of the monitor.
+    /// </returns>
+    public (float xScale, float yScale)? GetMonitorContentScale(IntPtr monitor);
+
+    /// <summary>
+    /// Get the name of a monitor.
+    /// </summary>
+    /// <param name="monitor">
+    /// The handle to the monitor to get the name of.
+    /// </param>
+    /// <returns>
+    /// The name of the monitor.
+    /// </returns>
+    public string? GetMonitorName(IntPtr monitor);
+
+    /// <summary>
+    /// Set the user pointer of a monitor.
+    /// </summary>
+    /// <param name="monitor">
+    /// The handle to the monitor to set the user pointer of.
+    /// </param>
+    /// <param name="userPtr">
+    /// The user pointer to set for the monitor.
+    /// </param>
+    public void SetMonitorUserPointer(IntPtr monitor, IntPtr userPtr);
+
+    /// <summary>
+    /// Get the user pointer of a monitor.
+    /// </summary>
+    /// <param name="monitor">
+    /// The handle to the monitor to get the user pointer of.
+    /// </param>
+    /// <returns>
+    /// The user pointer of the monitor.
+    /// </returns>
+    public IntPtr GetMonitorUserPointer(IntPtr monitor);
+
+    /// <summary>
+    /// Set the callback to invoke when a monitor is connected or disconnected
+    /// from the system.
+    /// </summary>
+    /// <param name="callback">
+    /// The callback to invoke when a monitor is connected or disconnected from
+    /// the system or <c>null</c> to clear the current callback.
+    /// </param>
+    /// <returns>
+    /// The previously set callback or <c>null</c> if no callback was previously
+    /// set.
+    /// </returns>
+    public MonitorCallback? SetMonitorCallback(MonitorCallback? callback);
+
+    /// <summary>
+    /// Get the available video modes for a monitor.
+    /// </summary>
+    /// <param name="monitor">
+    /// The handle to the monitor to get the available video modes for.
+    /// </param>
+    /// <returns>
+    /// The available video modes for the monitor.
+    /// </returns>
+    public VideoMode[] GetVideoModes(IntPtr monitor);
+
+    /// <summary>
+    /// Get the current video mode of a monitor.
+    /// </summary>
+    /// <param name="monitor">
+    /// The handle to the monitor to get the current video mode of.
+    /// </param>
+    /// <returns>
+    /// The current video mode of the monitor.
+    /// </returns>
+    public VideoMode? GetVideoMode(IntPtr monitor);
+
+    /// <summary>
+    /// Set the gamma of a monitor.
+    /// </summary>
+    /// <param name="monitor">
+    /// The handle to the monitor to set the gamma of.
+    /// </param>
+    /// <param name="gamma">
+    /// The gamma to set the monitor to.
+    /// </param>
+    public void SetGamma(IntPtr monitor, float gamma);
+
+    /// <summary>
+    /// Get the gamma ramp of a monitor.
+    /// </summary>
+    /// <param name="monitor">
+    /// The handle to the monitor to get the gamma ramp of.
+    /// </param>
+    /// <returns>
+    /// The gamma ramp of the monitor.
+    /// </returns>
+    public GammaRamp? GetGammaRamp(IntPtr monitor);
+
+    /// <summary>
+    /// Set the gamma ramp of a monitor.
+    /// </summary>
+    /// <param name="monitor">
+    /// The handle to the monitor to set the gamma ramp of.
+    /// </param>
+    /// <param name="ramp">
+    /// The gamma ramp to set the monitor to.
+    /// </param>
+    public void SetGammaRamp(IntPtr monitor, GammaRamp ramp);
 
     #endregion
 }
