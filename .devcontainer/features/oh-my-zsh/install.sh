460885c46dd4addd78a6afda86d8f29958c009f6
G'lek Tarssza <glek@glektarssza.com>
2025-02-24 20:23:53 +0000

    Updating dev container.
    Again.

--- .devcontainer/features/oh-my-zsh/install.sh
+++ .devcontainer/features/oh-my-zsh/install.sh
@@ -8,7 +8,7 @@ su ${_REMOTE_USER} -c 'sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/o
 # Ensure zsh profile exists for remote user.
 su ${_REMOTE_USER} -c "touch ~${_REMOTE_USER}/.zshrc" && \
 # Copy any custom themes from the container to our local install.
-su ${_REMOTE_USER} -c "cp /usr/share/zsh-themes/* ~${_REMOTE_USER}/.oh-my-zsh/custom/themes/" && \
+su ${_REMOTE_USER} -c "cp ./themes/*.zsh-theme ~${_REMOTE_USER}/.oh-my-zsh/custom/themes/" && \
 # Set oh-my-zsh theme for the remote user
 su ${_REMOTE_USER} -c "zsh -ic \"omz theme set ${THEME}\"" && \
 # Enable oh-my-zsh plugins for the remote user
