dda622b346c5767a838b920d4000063241bc063f
G'lek Tarssza <glek@glektarssza.com>
2025-02-24 12:04:56 +0000

    Basic project layout.

---
+++ Glekcraft/src/Program.cs
@@ -0,0 +1,11 @@
+namespace Glekcraft;
+
+/// <summary>
+/// The class containing the program entry point.
+/// </summary>
+public class Program {
+    /// <summary>
+    /// The program entry point.
+    /// </summary>
+    public static void Main() => Console.WriteLine("Hello, World!");
+}
