7891075c7d0c24adb650d0df83e7d9c1ab6bc809
G'lek Tarssza <glek@glektarssza.com>
2025-02-26 15:19:17 +0000

    Little bit more cleanup.

--- scripts/decrypt-secret.sh
+++ scripts/decrypt-secret.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
 # Ensure output directory exists before using it
-mkdir -p ${HOME}/.secrets;
+mkdir -p "${HOME}/.secrets";
+
+# Set file name to first argument
+SECRET_FILE="${1}";
 
 # Determine our passphrase variable from our second argument
 eval "SECRET_PASSPHRASE=\${$2}";
 
 # Decrypt the secret given in the first argument
 gpg --quiet --yes --batch --decrypt --passphrase="${SECRET_PASSPHRASE}" \
-    --output ${HOME}/.secrets/${1##.gpg} ${1};
+    --output "${HOME}/.secrets/${SECRET_FILE##.gpg}" "${SECRET_FILE}";
 
 # Store our result
-RESULT=$?
+RESULT=$?;
 
-# Clear our secret passphrase
+# Unset variables before exiting
+unset SECRET_FILE;
 unset SECRET_PASSPHRASE;
 
 # Exit with the result code from gpg
