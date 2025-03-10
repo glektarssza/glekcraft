f67c6f64b2e1113fef2393e3bd7050bf825758ca
G'lek Tarssza <glek@glektarssza.com>
2025-03-09 18:14:06 -0600

    Some more unit tests.

--- tests/Graphics.GLFW/src/LibGLFWTests.cs
+++ tests/Graphics.GLFW/src/LibGLFWTests.cs
@@ -11,28 +11,99 @@ public class LibGLFWTests {
 
     #endregion
 
-    #region Private Properties
-
-    /// <summary>
-    /// The mock native API provider.
-    /// </summary>
-    private INativeAPIProvider MockNativeAPIProvider =>
-        this.MoqNativeAPIProvider.Object;
-
-
-    #endregion
+    [TestCleanup]
+    public void AfterEach() =>
+        LibGLFW.Instance?.Dispose();
 
     [TestMethod]
     [TestCategory("Core")]
     [Description("Test whether the 'Initialize' static method returns a new instance when the library is not initialized.")]
-    public void Test_Initialize_ReturnsNewInstance() {
+    public void Test_Initialize_ReturnsNewInstanceWhenNotInitialized() {
         //-- Given
         this.MoqNativeAPIProvider.Setup((it) => it.Init()).Returns(true);
 
         //-- When
-        var result = FluentActions.Invoking(() => LibGLFW.Initialize(this.MockNativeAPIProvider));
+        var result = FluentActions.Invoking(() =>
+            LibGLFW.Initialize(this.MoqNativeAPIProvider.Object));
 
         //-- Then
-        using var instance = result.Should().NotThrow().Subject;
+        result.Should().NotThrow();
+    }
+
+    [TestMethod]
+    [TestCategory("Core")]
+    [Description("Test whether the 'Initialize' static method returns the same instance when the library is already initialized.")]
+    public void Test_Initialize_ReturnsSameInstanceWhenAlreadyInitialized() {
+        //-- Given
+        this.MoqNativeAPIProvider.Setup((it) => it.Init()).Returns(true);
+        using var instance = LibGLFW.Initialize(this.MoqNativeAPIProvider.Object);
+
+        //-- When
+        var result = FluentActions.Invoking(() =>
+            LibGLFW.Initialize(this.MoqNativeAPIProvider.Object));
+
+        //-- Then
+        using var instance2 = result.Should().NotThrow().Subject;
+        instance2.Should().BeSameAs(instance);
+    }
+
+    [TestMethod]
+    [TestCategory("Core")]
+    [Description("Test whether the 'IsInitialized' static property is 'false' when the library is not initialized.")]
+    public void Test_IsInitialized_IsFalseWhenLibraryIsNotInitialized() {
+        //-- Given
+        this.MoqNativeAPIProvider.Setup((it) => it.Init()).Returns(true);
+
+        //-- When
+        var result = LibGLFW.IsInitialized;
+
+        //-- Then
+        result.Should().BeFalse();
+    }
+
+    [TestMethod]
+    [TestCategory("Core")]
+    [Description("Test whether the 'IsInitialized' static property is 'true' when the library is initialized.")]
+    public void Test_IsInitialized_IsTrueWhenLibraryIsInitialized() {
+        //-- Given
+        this.MoqNativeAPIProvider.Setup((it) => it.Init()).Returns(true);
+        using var instance = LibGLFW.Initialize(this.MoqNativeAPIProvider.Object);
+
+        //-- When
+        var result = LibGLFW.IsInitialized;
+
+        //-- Then
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    [TestCategory("Core")]
+    [Description("Test whether the 'IsCurrentInstance' property is 'false' when the instance is not the current instance.")]
+    public void Test_IsCurrentInstance_IsFalseWhenInstanceIsNotCurrent() {
+        //-- Given
+        this.MoqNativeAPIProvider.Setup((it) => it.Init()).Returns(true);
+        using var instance = LibGLFW.Initialize(this.MoqNativeAPIProvider.Object);
+
+        //-- When
+        instance.Dispose();
+        var result = instance.IsCurrentInstance;
+
+        //-- Then
+        result.Should().BeFalse();
+    }
+
+    [TestMethod]
+    [TestCategory("Core")]
+    [Description("Test whether the 'IsCurrentInstance' property is 'true' when the instance is the current instance.")]
+    public void Test_IsCurrentInstance_IsTrueWhenInstanceIsCurrent() {
+        //-- Given
+        this.MoqNativeAPIProvider.Setup((it) => it.Init()).Returns(true);
+        using var instance = LibGLFW.Initialize(this.MoqNativeAPIProvider.Object);
+
+        //-- When
+        var result = instance.IsCurrentInstance;
+
+        //-- Then
+        result.Should().BeTrue();
     }
 }
