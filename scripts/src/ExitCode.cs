9790455cb472558b4faeb722a60e381576b392c5
G'lek Tarssza <glek@glektarssza.com>
2026-01-07 13:43:08 -0700

    Set up scripts project.

---
+++ scripts/src/ExitCode.cs
@@ -0,0 +1,16 @@
+namespace Glekcraft.Scripts;
+
+/// <summary>
+/// An enumeration of supported exit codes.
+/// </summary>
+public enum ExitCode : int {
+    /// <summary>
+    /// The program exited normally.
+    /// </summary>
+    Ok = 0,
+
+    /// <summary>
+    /// The program exited with an error.
+    /// </summary>
+    Error = 1
+}
---
+++ scripts/src/Program.cs
@@ -0,0 +1,17 @@
+namespace Glekcraft.Scripts;
+
+/// <summary>
+/// The class containing the program entry point.
+/// </summary>
+public class Program {
+    /// <summary>
+    /// The program entry point.
+    /// </summary>
+    /// <returns>
+    /// A status code for the operating system.
+    /// </returns>
+    public static int Main() {
+        Console.WriteLine("Hello, World!");
+        return (int)ExitCode.Ok;
+    }
+}
